4a57e905ecf9b2e63ed0722efec920f4948207bc
Kastelirac <kasteliracc@gmail.com>
2019-11-17 23:57:27 +0100

    Update registrirajse.vue

--- src/components/registrirajse.vue
+++ src/components/registrirajse.vue
@@ -1,7 +1,79 @@
 <template>
-    <h1>registrirajse</h1>
-</template>
+    <div class="container">
+        <br/><br/><br/><br/><br/>
+        <section class="main-section">
+            <div class="circle-main"></div>
+            <div class="circle-main-m"></div>
+            <div class="circle-main-s"></div>
 
+            <div class="container white-bg">
+            <div class="columns background component-row">
+                <div class="column">
+                <div class="padable">
+                    <h1>Registriraj se <span class="line"></span></h1>
+
+                    <div class="field">
+                        <div class="control">
+                            <div class="control has-icons-left">
+                            <input class="input is-rounded is-medium is-success" type="text" placeholder="Vaše korisničko ime">
+                                <span class="icon is-small is-left">
+                                    <i class="fa fa-user"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <p class="help is-success">Uneseno ime je dostupno</p>
+                    </div>
+
+                    <div class="field">
+                        <div class="control">
+                            <div class="control has-icons-left">
+                            <input class="input is-danger is-rounded is-medium" type="email" placeholder="Email" >
+                                <span class="icon is-small is-left">
+                                    <i class="fa fa-envelope"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <p class="help is-danger">This email is invalid</p>
+                    </div>
+
+                    <div class="field">
+                    <div class="control">
+                        <div class="control has-icons-left">
+                            <input class="input is-rounded is-medium" type="password" placeholder="***********">
+                            <span class="icon is-small is-left">
+                                <i class="fa fa-lock"></i>
+                            </span>
+                        </div>                          
+                    </div>
+                    </div>
+                    <div class="pls">
+                        <div class="field">
+                            <div class="control">
+                                <label class="checkbox">
+                                <input type="checkbox">
+                                    Slažem se sa <a href="#">uvijetima i odredbama</a>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="control">
+                            <button class="button is-warning is-rounded">Napravite korisnički račun</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+
+        </section>
+    </div>
+</template>
+<style scoped>
+    .pls{
+        text-align:center;
+    }
+</style>
 <script>
 export default{
     
