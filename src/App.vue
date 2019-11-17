74d3f8c808ebf6a9755d0d832e8e1309e56d40a6
Kastelirac <kasteliracc@gmail.com>
2019-11-17 23:57:17 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -57,21 +57,7 @@
                 </div>
             </nav>
             </div>
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            <div class="hero-body has-text-white">
-                <div class="container has-text-centered">
-                    <div class="column is-6 is-offset-3">
-                        <h1 id="h1">
-                          Dosadno mi je... Baš bih nešto radio.
-                        </h1>
-                        <h2 id="h2">
-                          Pronalazak aktivnosti nikad nije bio lakši
-                        </h2>
-                        <button class="button is-info is-medium"><router-link to="pronadi">Pronađi aktivnosti</router-link></button>
 
-                    </div>
-                </div>
-          </div> 
     <router-view/>
   </div>
 </template>
@@ -89,19 +75,7 @@ export default {
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-
-        background-image: url(./assets/wallpaper.jpg);
-        background-size: cover;
-        background-repeat: no-repeat;
-
       }
 
-      #h1{
-        font-size:35px;
-        font-weight: bold;
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-      }
-      #h2{
-        font-size:25px;
-      }
+
 </style>
