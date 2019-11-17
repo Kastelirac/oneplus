f9d2ea45643cc195e900dd926f3337ab462d7727
Kastelirac <kasteliracc@gmail.com>
2019-11-17 23:57:29 +0100

    Create trazi.vue

---
+++ src/components/trazi.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <br/><br/><br/><br/><br/>
+        <p> Ovdje bi trebalo doc koje su vrijeme i mjesto odabrali</p>
+        <div class="tabs is-centered">
+        <ul>
+            <li class="is-focused">
+                <a>
+                    <span class="icon is-small"><i class="fas fa-image" aria-hidden="true"></i></span>
+                    <span>Sve</span>
+                </a>
+            </li>
+            <li>
+                <a>
+                    <span class="icon is-small"><i class="fa fa-soccer-ball-o" aria-hidden="true"></i></span>
+                    <span>Sport</span>
+                </a>
+            </li>
+            <li>
+                <a>
+                    <span class="icon is-small"><i class="fas fa-chess" aria-hidden="true"></i></span>
+                    <span>Drustvene igre</span>
+                </a>
+            </li>
+            <li>
+                <a>
+                    <span class="icon is-small"><i class="fa fa-gamepad" aria-hidden="true"></i></span>
+                    <span>E-sport</span>
+                </a>
+            </li>
+            <li>
+                <a>
+                    <span class="icon is-small"><i class="far fa-file-alt" aria-hidden="true"></i></span>
+                    <span>Random</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div> 
+</template>
