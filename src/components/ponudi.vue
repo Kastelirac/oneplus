6764bd4f861ea7f87cf3839954398b85160cc367
Kastelirac <kasteliracc@gmail.com>
2019-11-17 23:57:21 +0100

    Update ponudi.vue

--- src/components/ponudi.vue
+++ src/components/ponudi.vue
@@ -1,15 +1,74 @@
 
 <template>
-        <article class="hero-body has-background-dark">
-            <div class="container">
-                <h1 class="title has-text-white-ter has-text-centered">Stranica</h1>
-                <h2 class="subtitle has-text-centered">Podnaslov</h2>
+    <div class="container">
 
-                <div class="columns is-mobile">
-                    <div class="column is-half is-offset-one-quarter has-background-light"></div>
-                </div>
+        <h1>Što želiš raditi?</h1>
+        <nav class="panel">
+            <p class="panel-heading">
+                Aktivnosti
+            </p>
+            <div class="panel-block">
+                <p class="control has-icons-left">
+                <input class="input" type="text" placeholder="Search">
+                <span class="icon is-left">
+                    <i class="fas fa-search" aria-hidden="true"></i>
+                </span>
+                </p>
             </div>
-        </article>
+            <p class="panel-tabs">
+                <a class="is-active">Sve</a>
+                <a>Sport</a>
+                <a>Društvene igre</a>
+                <a>E-sportovi</a>
+                <a>Random</a>
+            </p>
+            <a class="panel-block is-active">
+                <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                Mali nogomet
+            </a>
+            <a class="panel-block">
+                <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                marksheet
+            </a>
+            <a class="panel-block">
+                <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                minireset.css
+            </a>
+            <a class="panel-block">
+                <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                jgthms.github.io
+            </a>
+            <a class="panel-block">
+                <span class="panel-icon">
+                <i class="fas fa-code-branch" aria-hidden="true"></i>
+                </span>
+                daniellowtw/infboard
+            </a>
+            <a class="panel-block">
+                <span class="panel-icon">
+                <i class="fas fa-code-branch" aria-hidden="true"></i>
+                </span>
+                mojs
+            </a>
+            <label class="panel-block">
+                <input type="checkbox">
+                remember me
+            </label>
+            <div class="panel-block">
+                <button class="button is-link is-outlined is-fullwidth">
+                Reset all filters
+                </button>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
