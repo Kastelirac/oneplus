e7247197a01094fb69f188746a7cc97a0eb2f22e
Kastelirac <kasteliracc@gmail.com>
2019-11-17 23:57:31 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+// import bulmaCalendar from 'bulma-calendar/dist/js/bulma-calendar.min.js'
 
 
 Vue.use(Buefy)
@@ -17,3 +18,25 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+/*new Vue({
+  el: '#app',
+  data() {
+    return {
+      date: new Date(),
+    }
+  },
+  mounted() {
+    const calendar = bulmaCalendar.attach(this.$refs.calendarTrigger, {
+      startDate: this.date,
+    })[0]
+    calendar.on('date:selected', e => (this.date = e.start || null))
+  },
+  computed: {
+    niceDate() {
+      if (this.date) {
+        return this.date.toLocaleDateString()
+      }
+    }
+  }
+}); */
