5cdb289158a7e08df3890b375321b7b1791e48ab
Kastelirac <kasteliracc@gmail.com>
2019-11-18 03:02:25 +0100

    Update prijava.vue

--- src/components/prijava.vue
+++ src/components/prijava.vue
@@ -1,17 +1,17 @@
 <template>
-<section class="hero is-fullheight">
+<section class="hero is-fullheight container">
         <div class="hero-body">
             <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
+                <div class="column is-6 is-offset-3">
                     <h3 class="title has-text-black">Prijavi se</h3>
                     <hr class="login-hr">
                     <p class="subtitle has-text-black">Prijavite se kako bi nastavili.</p>
-                    <div class="box">
+                    <div class="">
                         <form>
                             <div class="field">
                                 <div class="control">                        
                                     <div class="control has-icons-left">
-                                        <input class="input is-medium" type="email" placeholder="Vaš mail" autofocus="">
+                                        <input class="input is-rounded is-medium" type="email" placeholder="Vaš mail" autofocus="">
                                         <span class="icon is-small is-left">
                                             <i class="fa fa-envelope"></i>
                                         </span>
@@ -22,7 +22,7 @@
                             <div class="field">
                                 <div class="control">
                                     <div class="control has-icons-left">
-                                    <input class="input is-medium" type="password" placeholder="Vaša lozinka">
+                                    <input class="input is-rounded is-medium" type="password" placeholder="Vaša lozinka">
                                         <span class="icon is-small is-left">
                                             <i class="fa fa-lock"></i>
                                         </span>
@@ -35,7 +35,7 @@
                   Zapamti me
                 </label>
                             </div>
-                            <button class="button is-block is-info is-large is-fullwidth">Prijavi se <i class="fa fa-sign-in" aria-hidden="true"></i></button>
+                            <button class="button is-rounded is-block is-info is-large is-fullwidth">Prijavi se <i class="fa fa-sign-in" aria-hidden="true"></i></button>
                         </form>
                     </div>
                     <p class="has-text-grey">
