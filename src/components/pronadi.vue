e770096de02e6be0ce7df20d06dc01168aa89e4d
Kastelirac <kasteliracc@gmail.com>
2019-11-19 11:20:42 +0100

    Update pronadi.vue

--- src/components/pronadi.vue
+++ src/components/pronadi.vue
@@ -4,8 +4,16 @@
         <link href="../../node_modules/bulma-calendar/dist/css/bulma-calendar.min.css" rel="stylesheet">
         <br/><br/><br/><br/><br/><br/><br/>
         <h1>Odaberite mjesto i vrijeme </h1><br/>
-        <label class="label">Kada?</label><br/>
-        <input type="date"><br/><br/>
+        <b-field label="Kada?">
+            <b-datetimepicker
+                rounded
+                placeholder="Click to select..."
+                icon="calendar-today"
+                :datepicker="{ showWeekNumber }"
+                :timepicker="{ enableSeconds, hourFormat: format }">
+            </b-datetimepicker>
+        </b-field>
+        <br/><br/>
         <div class="field">
             <label class="label">Gdje?</label>
             <div class="control">
@@ -13,19 +21,19 @@
             </div>
         </div>
         <button class="button is-info is-medium"><router-link to="trazi">Traži</router-link></button>
-        <!-- <div id='app'>
-            <button ref='calendarTrigger' type='button'>Change</button>
-        </div> -->
-
+        
 
     </div>
     
 </template>
 <script src="https://kit.fontawesome.com/18ca921f41.js" crossorigin="anonymous"></script>
-<!-- <script src="../../node_modules/bulma-calendar/dist/js/bulma-calendar.min.js"></script> -->
 <script>
-export default{
-
+export default {
+    data() {
+        return {
+            showWeekNumber: false
+        }
+    }
 }
 </script>
 
