94e895591830b73d031474c21be53e9a6856f828
Kastelirac <kasteliracc@gmail.com>
2019-12-12 11:47:45 +0100

    Update pronadi.vue

--- src/components/pronadi.vue
+++ src/components/pronadi.vue
@@ -1,32 +1,52 @@
 <template>
-    <div class="container">
-        <br/><br/><br/><br/><br/>        <link rel="stylesheet" href="https://kit.fontawesome.com/18ca921f41.js">
-        <link href="../../node_modules/bulma-calendar/dist/css/bulma-calendar.min.css" rel="stylesheet">
+    <div class="max">
+        <br/><br/><br/><br/><br/>        
         <br/><br/><br/><br/><br/><br/><br/>
         <h1>Odaberite mjesto i vrijeme </h1><br/>
-        <b-field label="Kada?">
+        <!--<b-field label="Kada?">
             <b-datetimepicker
                 rounded
-                placeholder="Click to select..."
+                placeholder="Odaberite vrijeme  "
                 icon="calendar-today"
                 :datepicker="{ showWeekNumber }"
                 :timepicker="{ enableSeconds, hourFormat: format }">
             </b-datetimepicker>
+        </b-field>-->
+        <b-field label="Odaberite datum">
+            <b-datepicker
+                rounded
+                placeholder="Odaberite datum"
+                icon="calendar-today"
+                editable>
+            </b-datepicker>
+        </b-field>
+        <b-field>
+        <b-clockpicker
+            rounded
+            placeholder="Click to select..."
+            icon="clock"
+            :hour-format="format">
+        </b-clockpicker>
         </b-field>
         <br/><br/>
-        <div class="field">
+        <b-field><br/>
             <label class="label">Gdje?</label>
             <div class="control">
-                <input class="input" type="text" placeholder="Unesite ime mjesta">
+                <b-input rounded class="input" type="text" placeholder="Unesite ime mjesta"></b-input> 
             </div>
-        </div>
+        </b-field>
         <button class="button is-info is-medium"><router-link to="trazi">Traži</router-link></button>
         
 
     </div>
     
 </template>
-<script src="https://kit.fontawesome.com/18ca921f41.js" crossorigin="anonymous"></script>
+<style scoped>
+    .max{
+        margin:0 auto;
+        max-width:300px;
+    }
+</style>
 <script>
 export default {
     data() {
