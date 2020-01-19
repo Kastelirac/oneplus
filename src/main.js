8b402cc40617322d65da9c8d4ba5deb9731eec27
Kastelirac <kasteliracc@gmail.com>
2020-01-19 11:56:17 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,15 +5,22 @@ import App from './App'
 import router from './router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
-
+import * as VueGoogleMaps from 'vue2-google-maps'
+import { store } from './store'
 
 Vue.use(Buefy)
 Vue.config.productionTip = false
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAWFxR54TgyQY4ipfjIJQH_uZ0KAfgN92k',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+  }})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
