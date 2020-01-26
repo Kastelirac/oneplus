9d450cc37861820a6ac63eef617e292e759ceff9
Kastelirac <kasteliracc@gmail.com>
2020-01-26 18:27:06 +0100

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as firebase from 'firebase'
 
 Vue.use(Vuex)
 
@@ -10,52 +11,160 @@ export const store = new Vuex.Store({
             id:'fef',
             troskovi:'15kn',
             brojosoba: '8',
-            komentar:'ulaz sa straznje strane',
-            naziv:'nogomet',
-            date: '2019-9-19',
+            komentar:'Ulaz sa straznje strane',
+            naziv:'Nogomet',
+            date: new Date(),
+            timestart: new Date(),
+            timeend: new Date(),
+            kategorija:'Sport',
             imageUrl:'https://picsum.photos/1000/700'
         },
         {
-            id:'njnje',
-            troskovi:'20kn',
-            brojosoba: '2',
-            komentar:'ulaz ispred zgrade',
-            naziv:'bella',
-            date: '2020-31-1',
-            imageUrl:'https://picsum.photos/200/300'
+            id:'fef',
+            troskovi:'30kn',
+            brojosoba: '4',
+            komentar:'Uzeti rucnike',
+            naziv:'Kosarka',
+            date: new Date(),
+            timestart: new Date(),
+            timeend: new Date(),
+            kategorija:'Sport',
+            imageUrl:'https://picsum.photos/1000/700'
         },
-        {
-            id:'njje',
-            troskovi:'10kn',
-            brojosoba: '2',
-            komentar:'dodite 5 minuta prije',
-            naziv:'macevanje',
-            date: '2020-29-1',
-            imageUrl:'https://picsum.photos/200/300'
-        }
         ],
-        user:{
-            id: 'haha',
-            registeredMeetups: ['njnje']
-        }
+        user:null,
+        loading: false,
+        error: null
     },
     mutations: {
-        createMeetup (state, payload){
+        createMeetup (state, payload) {
             state.loadedMeetups.push(payload)
+          },
+        setLoadedMeetups (state, payload) {
+          state.loadedMeetups = payload
+        },
+        setUser (state, payload) {
+          state.user = payload
+        },
+        setLoading (state, payload) {
+          state.loading = payload
+        },
+        setError (state, payload) {
+          state.error = payload
+        },
+        clearError (state) {
+          state.error = null
         }
-    },
+      },
     actions: {
+        loadMeetups ({commit}) {
+            commit('setLoading', true)
+            firebase.database().ref('meetups').once('value')
+              .then((data) => {
+                const meetups = []
+                const obj = data.val()
+                for (let key in obj) {
+                  meetups.push({
+                    id: key,
+                    naziv: obj[key].naziv,
+                    komentar: obj[key].komentar,
+                    troskovi: obj[key].troskovi,
+                    date: obj[key].date,
+                    creatorId: obj[key].creatorId,
+                    brojosoba: obj[key].brojosoba,
+                    timestart: obj[key].timestart,
+                    timeend: obj[key].timeend,
+                    lat: obj[key].lat,
+                    lng: obj[key].lng,
+                    kategorija: obj[key].kategorija,
+                  })
+                }
+              })
+              .catch(
+                (error) => {
+                  console.log(error)
+                  commit('setLoading', false)
+                }
+              )
+          },
         createMeetup({commit}, payload){
             const meetup = {
                 troskovi: payload.troskovi,
                 brojosoba: payload.brojosoba,
                 komentar: payload.komentar,
                 naziv: payload.naziv,
-                datum: payload.datum,
-                id: 'bezveze'
+                date: payload.date,
+                timestart: payload.timestartdate,
+                timeend:payload.timeenddate,
+                lat: payload.lat,
+                lng: payload.lng,
+                kategorija: payload.kategorija,
             }
-            commit ('createMeetup', meetup)
-        }
+            commit('createMeetup', meetup)
+        },
+            /*firebase.database().ref('meetups').push(meetup)
+            .then((data) => {
+              const key = data.key
+              commit('createMeetup', {
+                ...meetup,
+                id: key
+              })
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+            }, */
+            signUserUp ({commit}, payload) {
+                firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+                  .then(
+                    user => {
+                      const newUser = {
+                        id: user.uid,
+                        registeredMeetups: []
+                      }
+                      commit('setUser', newUser)
+                    }
+                  )
+                  .catch(
+                    error => {
+                    commit('setLoading', false)
+                    commit('setError', error)
+                    console.log(error)
+                    }
+                )
+            },
+            signUserIn ({commit}, payload) {
+                commit('setLoading', true)
+                commit('clearError')
+                firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+                  .then(
+                    user => {
+                      commit('setLoading', false)
+                      const newUser = {
+                        id: user.uid,
+                        registeredMeetups: []
+                      }
+                      commit('setUser', newUser)
+                    }
+                  )
+                  .catch(
+                    error => {
+                      commit('setLoading', false)
+                      commit('setError', error)
+                      console.log(error)
+                    }
+                  )
+              },
+              autoSignIn ({commit}, payload) {
+                commit('setUser', {id: payload.uid, registeredMeetups: []})
+              },
+              logout ({commit}) {
+                firebase.auth().signOut()
+                commit('setUser', null)
+              },
+              clearError ({commit}) {
+                commit('clearError')
+              } 
     },
     getters: {
         loadedMeetups (state){
@@ -72,6 +181,12 @@ export const store = new Vuex.Store({
                     return meetup.id === meetupId
                 })
             }
-        }
+        },
+        user (state) {
+            return state.user
+          },
+          loading (state) {
+            return state.loading
+          },
     }
 })
